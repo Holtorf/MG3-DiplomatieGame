9eb3d506131608eb1041b50ef277e141fee6ada3
Bettina <bettina.ganter@gmx.de>
2018-06-02 17:58:17 +0200

    Network Lobby

---
+++ Application/Assets/Scripts/ParameterTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class ParameterTests : NetworkBehaviour {
+
+    public const int maxTechValue = 100;
+    [SyncVar]
+    public int currentTechValue = 0;
+
+    public Button button;
+
+    public RectTransform techBar;
+
+    public void Start()
+    {
+        Button btn;
+        btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(increaseTechValue);
+
+    }
+
+    public void increaseTechValue()
+    {
+        currentTechValue += 10;
+
+        if(!isServer)
+        techBar.sizeDelta = new Vector2(currentTechValue, techBar.sizeDelta.y);
+    }
+
+    
+}
